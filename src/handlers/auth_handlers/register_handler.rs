74762163176b043c5db65b9d947c2818a4c63872
Vinmar Tipay <vinmar.tipay@outlook.com>
2025-08-16 15:36:27 +0000

    done with register

--- src/handlers/auth_handlers/register_handler.rs
+++ src/handlers/auth_handlers/register_handler.rs
@@ -3,11 +3,13 @@ use actix_web::{HttpRequest, Responder, web};
 use serde::{Deserialize, Serialize};
 use sqlx::MySqlPool;
 
-use crate::models::user_auth_identity_model::AuthIdentity;
+use crate::models::user_auth_identity_model::UserAuthIdentity;
+use crate::models::user_email_model::UserEmail;
 use crate::models::user_model::User;
-use crate::services::user_service::register_new_user;
+use crate::services::user_service::create_user;
 use crate::utils::json_response_utils::{JsonGeneralResponse, JsonJwtResponse};
 use crate::utils::jwt_utils;
+use crate::utils::string_utils::{is_alphabet_only, is_email_format};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct RegisterRequestData {
@@ -23,102 +25,145 @@ pub async fn register(
     pool: web::Data<MySqlPool>,
     json_data: web::Json<RegisterRequestData>,
 ) -> impl Responder {
-    // Check email
-    match User::get_user_by_email(&pool, &json_data.email).await {
-        Err(e) => {
-            log::error!("Error while checking for email. {}", e);
-            return JsonGeneralResponse::make_response(
-                &req,
-                &StatusCode::INTERNAL_SERVER_ERROR,
-                "Server error. Try again later.",
-            );
-        }
-        Ok(Some(_)) => {
-            return JsonGeneralResponse::make_response(
-                &req,
-                &StatusCode::CONFLICT,
-                &String::from("Email address already in use"),
-            );
-            // return HttpResponse::Conflict().body("Email address already in use");
-        }
-        Ok(None) => (),
-    }
+    // ---- Request Input Data Validation ----
 
-    // Check username
-    match User::get_user_by_username(&pool, &json_data.username).await {
-        Err(e) => {
-            log::error!("Error while checking for username. {}", e);
-            return JsonGeneralResponse::make_response(
-                &req,
-                &StatusCode::INTERNAL_SERVER_ERROR,
-                "Server error. Try again later.",
-            );
-        }
-        Ok(Some(_)) => {
-            return JsonGeneralResponse::make_response(
-                &req,
-                &StatusCode::CONFLICT,
-                &String::from("Username already in use"),
-            );
-            // return HttpResponse::Conflict().body("Username already in use");
-        }
-        Ok(None) => (),
-    }
-
-    // Check password and repeat matched
-    if json_data.password != json_data.repeat {
+    // -- Use this for now but change it in the future for more robust validation
+    // validation
+    // firstname
+    if is_alphabet_only(&json_data.firstname) == false {
         return JsonGeneralResponse::make_response(
             &req,
             &StatusCode::BAD_REQUEST,
-            &String::from("Password did not match"),
+            "Firstname must only contain Alphabet characters",
         );
-        // return HttpResponse::BadRequest().body("Password did not match");
     }
 
-    // register new user
-    let user = match register_new_user(&pool, &json_data).await {
+    if json_data.firstname.len() < 1 || json_data.firstname.len() > 64 {
+        return JsonGeneralResponse::make_response(
+            &req,
+            &StatusCode::BAD_REQUEST,
+            "Firstname lenght must be between 1 and 64 characters",
+        );
+    }
+
+    // lastname
+    if is_alphabet_only(&json_data.lastname) == false {
+        return JsonGeneralResponse::make_response(
+            &req,
+            &StatusCode::BAD_REQUEST,
+            "Lastname must only contain Alphabet characters",
+        );
+    }
+
+    if json_data.lastname.len() < 1 || json_data.lastname.len() > 64 {
+        return JsonGeneralResponse::make_response(
+            &req,
+            &StatusCode::BAD_REQUEST,
+            "Lastname lenght must be between 1 and 64 characters",
+        );
+    }
+
+    // email
+    if is_email_format(&json_data.email) == false {
+        return JsonGeneralResponse::make_response(
+            &req,
+            &StatusCode::BAD_REQUEST,
+            "Email address has an invalid format",
+        );
+    }
+
+    // password
+    if json_data.password.len() < 8 {
+        return JsonGeneralResponse::make_response(
+            &req,
+            &StatusCode::BAD_REQUEST,
+            "Password must be atleast 8 characters long",
+        );
+    }
+
+    // repeat password
+    if json_data.repeat != json_data.password {
+        return JsonGeneralResponse::make_response(
+            &req,
+            &StatusCode::BAD_REQUEST,
+            "Password did not match",
+        );
+    }
+
+    // ---- User Validation ----
+
+    // Check email
+    match UserEmail::get_by_value(&pool, &json_data.email.trim().to_lowercase()).await {
         Err(e) => {
-            log::error!("Error when trying to register new user. {}", e);
+            log::error!("{}", e);
             return JsonGeneralResponse::make_response(
                 &req,
                 &StatusCode::INTERNAL_SERVER_ERROR,
-                "Server error. Try again later.",
+                "Server error, try again later",
             );
         }
-        Ok(user) => user,
+        Ok(Some(ue)) => {
+            // Check if email is in use
+            match User::get_by_email_id(&pool, ue.id).await {
+                Err(e) => {
+                    log::error!("{}", e);
+                    return JsonGeneralResponse::make_response(
+                        &req,
+                        &StatusCode::INTERNAL_SERVER_ERROR,
+                        "Server error, try again later",
+                    );
+                }
+                Ok(Some(_)) => {
+                    return JsonGeneralResponse::make_response(
+                        &req,
+                        &StatusCode::CONFLICT,
+                        "Email address is already in use",
+                    );
+                }
+                Ok(None) => {}
+            }
+        }
+        Ok(None) => {}
+    }
+
+    // ----- Create User -----
+    let user = match create_user(&pool, &json_data).await {
+        Err(e) => {
+            log::error!("{}", e);
+            return JsonGeneralResponse::make_response(
+                &req,
+                &StatusCode::INTERNAL_SERVER_ERROR,
+                "Server error, try again later",
+            );
+        }
+        Ok(u) => u,
     };
 
-    // get auth identity
-    let auth_identity = match AuthIdentity::get_by_id(&pool, user.auth_identity_id).await {
+    // ----- Create JWTokens -------
+
+    // get auth id
+    let user_ath_identity = match UserAuthIdentity::get_by_id(&pool, user.auth_identity_id).await {
         Err(e) => {
-            log::error!(
-                "Error when trying to get auth identity of newly created user. {}",
-                e
-            );
-
-            // user has been created so not really internal server error response anymore
-            // try a better response
+            log::error!("{}", e);
             return JsonGeneralResponse::make_response(
                 &req,
                 &StatusCode::INTERNAL_SERVER_ERROR,
-                "Server error. Try again later.",
+                "Server error, try again later",
             );
         }
-        Ok(Some(aio)) => aio,
         Ok(None) => {
-            // this should never happen unless something wrong with db, connection or register
-            log::error!("Empty row auth identity for newly created user.");
-
+            log::error!("Unable to get user auth identity id from newly created user");
             return JsonGeneralResponse::make_response(
                 &req,
                 &StatusCode::INTERNAL_SERVER_ERROR,
-                "Server error. Try again later.",
+                "Server error, try again later",
             );
         }
+        Ok(Some(uai)) => uai,
     };
 
     // generate tokens
-    let access_token = match jwt_utils::generate_access_token(&auth_identity.value) {
+    let access_token = match jwt_utils::generate_access_token(&user_ath_identity.value) {
         Ok(token) => token,
         Err(e) => {
             log::error!(
@@ -133,7 +178,7 @@ pub async fn register(
         }
     };
 
-    let refresh_token = match jwt_utils::generate_refresh_token(&auth_identity.value) {
+    let refresh_token = match jwt_utils::generate_refresh_token(&user_ath_identity.value) {
         Ok(token) => token,
         Err(e) => {
             log::error!(
@@ -148,5 +193,6 @@ pub async fn register(
         }
     };
 
+    // -- Generate and reposne with jwt access token and cookie refresh token
     JsonJwtResponse::make_response(&req, &StatusCode::CREATED, &access_token, &refresh_token)
 }
--- src/utils/string_utils.rs
+++ src/utils/string_utils.rs
@@ -1,3 +1,8 @@
+use std::str::FromStr;
+
+use email_address::EmailAddress;
+use regex::Regex;
+
 pub fn is_alphanumeric_or_underscore(s: &str) -> bool {
     s.chars().all(|c| c.is_alphanumeric() || c == '_')
 }
@@ -8,4 +13,63 @@ pub fn is_first_character_underscore(s: &str) -> bool {
     } else {
         false
     }
+}
+
+/// Checks if the input string contains only alphabetic characters (A-Z, a-z).
+///
+/// This function uses a regular expression to validate if the input string
+/// consists solely of alphabetic characters. The regular expression matches
+/// strings containing only uppercase and lowercase letters. If the regex
+/// fails to compile, an error message is logged, and the function returns `false`.
+///
+/// # Arguments
+///
+/// * `s` - A string slice that represents the input to check.
+///
+/// # Returns
+///
+/// * `true` if the string contains only alphabetic characters.
+/// * `false` otherwise, or if regex compilation fails.
+///
+/// # Example
+/// ```
+/// assert!(is_alphabet_only("Hello"));
+/// assert!(!is_alphabet_only("Hello123"));
+/// assert!(!is_alphabet_only("Hello@World"));
+/// ```
+pub fn is_alphabet_only(s: &str) -> bool {
+    let re = match Regex::new(r"^a-zA-Z+$") {
+        Ok(regex) => regex,
+        Err(_) => {
+            log::error!("Failed to compile regex");
+            return false;
+        }
+    };
+
+    re.is_match(s)
+}
+
+/// Validates whether the given string is a valid email format.
+///
+/// This function attempts to parse the input string as an email address using
+/// the `EmailAddress` type. If the parsing succeeds, it returns `true`; otherwise,
+/// it returns `false`. This function does not validate whether the email address
+/// actually exists, only if it follows the correct syntax.
+///
+/// # Arguments
+///
+/// * `email` - A string slice representing the email address to validate.
+///
+/// # Returns
+///
+/// * `true` if the email is in a valid format.
+/// * `false` otherwise.
+///
+/// # Example
+/// ```
+/// assert!(is_email_format("example@example.com"));
+/// assert!(!is_email_format("not-an-email"));
+/// ```
+pub fn is_email_format(email: &str) -> bool {
+    return EmailAddress::from_str(email).is_ok();
 }
