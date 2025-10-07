966ca8321d970726859e019fa07801386f80e741
Vinmar Tipay <vinmar.tipay@outlook.com>
2025-10-07 17:41:43 +0000

    added organisation_pid_model file and added it to mod.rs

---
+++ src/models/organisation_pid_model.rs
@@ -0,0 +1,9 @@
+use serde::Serialize;
+use sqlx::FromRow;
+
+#[derive(Debug, Serialize, FromRow)]
+pub struct OrganisationPIDModel {
+    pub id: u64,
+    pub datetime_created: NaiveDateTime,
+    pub value: String,
+}
