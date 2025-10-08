8886dd0522ef66c084729a0f5184cdd0fc2d40b7
Vinmar Tipay <vinmar.tipay@outlook.com>
2025-10-08 18:12:50 +0000

    implemented new associated function for OrganisationModel

--- src/models/organisation_model.rs
+++ src/models/organisation_model.rs
@@ -2,7 +2,7 @@ use chrono::NaiveDateTime;
 use serde::Serialize;
 use sqlx::{FromRow, MySql, Transaction};
 
-#[derive(Serialize, FromRow, Debug)]
+#[derive(Debug, FromRow, Serialize)]
 pub struct OrganisationModel {
     pub id: u64,
     pub datetime_created: NaiveDateTime,
@@ -10,4 +10,44 @@ pub struct OrganisationModel {
     pub description: String,
     pub datetime_deleted: Option<NaiveDateTime>,
     pub pid_id: u64,
+}
+
+impl OrganisationModel {
+    pub async fn new(
+        tx: &mut Transaction<'_, MySql>,
+        name: &str,
+        description: &str,
+        pid_id: u64,
+    ) -> Result<OrganisationModel, sqlx::error::Error> {
+        sqlx::query!(
+            r#"
+            INSERT INTO organisation (name, description, pid_id)
+            VALUES (?, ?, ?)
+            "#,
+            name,
+            description,
+            pid_id
+        )
+        .execute(&mut **tx)
+        .await?;
+
+        let row = sqlx::query_as!(
+            OrganisationModel,
+            r#"
+            SELECT 
+                id, 
+                datetime_created, 
+                name, 
+                description, 
+                datetime_deleted, 
+                pid_id
+            FROM organisation 
+            WHERE id = LAST_INSERT_ID()
+            "#
+        )
+        .fetch_one(&mut **tx)
+        .await?;
+
+        Ok(row)
+    }
 }
